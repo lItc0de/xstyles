24cc9e9d4ab0705c6bb759329e73b0c0991a83ed
litc0de <litc0de@posteo.org>
2018-11-15 23:35:49 +0100

    add custom webpack config for building lib

---
+++ config/webpack.prod.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const merge = require('webpack-merge');
+/* eslint-enable import/no-extraneous-dependencies */
+const { resolve } = require('path');
+
+const base = require('./webpack.base');
+
+module.exports = merge(base, {
+  entry: resolve(__dirname, '..', 'src', 'main.js'),
+
+  output: {
+    path: resolve(__dirname, '..', 'dist'),
+    filename: 'xstyles.common.js',
+    chunkFilename: '[id].js',
+    libraryTarget: 'commonjs2',
+  },
+});
