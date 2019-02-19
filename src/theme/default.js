dd284e266d841cb8576ffa7c86b3ebf63558e8da
litc0de <litc0de@posteo.org>
2019-02-19 15:00:12 +0100

    addendum to the last commit: adjust default colors

--- src/theme/default.js
+++ src/theme/default.js
@@ -1,19 +1,21 @@
 export default {
   colors: {
-    primary: '#777',
+    primary: '#f25f5c',
 
-    backgroundColor: '#fff', // #302D42
-    backgroundColorLight: 'rgba(255, 255, 255, .1)',
-    color: '#777',
+    backgroundColor: '#FAFAFA', // #302D42
+    backgroundColorLight: '#FFFFFF',
+    color: '#444',
     colorSecondary: '#aaa',
     textSecondary: '#aaa',
-    selection: 'red',
-    border: '#eee',
-    boxShadow: 'rgba(0, 0, 0, .3)',
+    selection: 'rgba(242, 92, 95, 0.3)',
+    border: '#ddd',
+    boxShadow: 'rgba(4, 4, 4, 0.1)',
   },
 
   sizes: {
     sizeBase: '16px',
+    boxShadowSpread: '0 0 40px',
+    borderRadius: '2px',
   },
 
   typography: {
