7202bcbb35bb4e193bdfb85df1d05929eb8df2de
litc0de <litc0de@posteo.org>
2019-03-10 23:16:25 +0100

    add icon btn component

---
+++ src/components/Btn/IconBtn.vue
@@ -0,0 +1,48 @@
+<template>
+  <button class="icon-btn" :style="style">
+    <svgicon
+      :icon="icon"
+      :color="color"
+      :height="height || scale"
+      :width="width || scale"
+      :title="title || icon"
+    />
+  </button>
+</template>
+
+<script>
+import XIcon from '../Icon/Icon.vue';
+import { colorMixin } from '../../mixins/styling';
+
+export default {
+  name: 'XIconBtn',
+
+  extends: XIcon,
+
+  mixins: [colorMixin],
+
+  computed: {
+    style() {
+      const { width, height, scale } = this;
+      const style = {
+        width: width || scale,
+        height: height || scale,
+      };
+
+      return { ...this.colorMixin, ...style };
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.icon-btn
+  box-sizing: content-box;
+  border-radius 50%
+  border none
+  cursor pointer
+  outline none
+  overflow hidden
+  padding .5rem
+  position relative
+</style>
