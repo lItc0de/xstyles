6bf3e6803fab10eb30fcc9e0df5af5479eac84f4
litc0de <litc0de@posteo.org>
2019-03-10 21:08:38 +0100

    refactore x-icon component to use svg

--- src/components/Icon/Icon.vue
+++ src/components/Icon/Icon.vue
@@ -1,31 +1,74 @@
 <template>
-  <i
-    :style="style"
-    :class="`icon mdi mdi-${$slots.default[0].text.trim()}`"
+  <svgicon
+    :icon="icon"
+    :color="color"
+    :dir="dir"
+    :fill="fill"
+    :height="height"
+    :width="width"
+    :scale="scale"
+    :title="title || icon"
   />
 </template>
 
 <script>
-import { fontMixin, colorMixin } from '../../mixins/styling';
-
 export default {
   name: 'XIcon',
 
-  mixins: [fontMixin, colorMixin],
-
-  computed: {
-    style() {
-      const style = {
-      };
-
-      return { ...this.colorMixin, ...this.fontMixin, ...style };
+  props: {
+    icon: {
+      type: String,
+      required: true,
     },
+
+    color: {
+      type: String,
+      default: 'var(--color)',
+    },
+
+    dir: {
+      type: [String, Object],
+      default: null,
+    },
+
+    fill: {
+      type: Boolean,
+      default: true,
+    },
+
+    height: {
+      type: String,
+      default: '1rem',
+    },
+
+    width: {
+      type: String,
+      default: '1rem',
+    },
+
+    scale: {
+      type: [String, Object],
+      default: '1',
+    },
+
+    title: {
+      type: [String, Object],
+      default: null,
+    },
+  },
+
+  methods: {
+    async importIcon() {
+      try {
+        await import(`@/assets/svg-icons/${this.icon}`);
+      } catch (error) {
+        console.warn(error.message, '| Please provide the svg icon');
+      }
+    },
+  },
+
+  created() {
+    this.importIcon();
   },
 };
 </script>
-
-<style lang="stylus" scoped>
-.icon
-  display inline-block
-  text-align center
-</style>
